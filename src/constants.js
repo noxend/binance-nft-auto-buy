6289a8584c22f99877eec4e2287d4fc048214f56
noxend <noxios5674@gmail.com>
2022-01-04 10:29:53 +0200

    .

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,19 @@
 const pages = {
-	LOGIN: "https://accounts.binance.com/en/login",
-	PRODUCT: (productId) =>
-		`https://www.binance.com/en/nft/goods/detail?productId=${productId}&isProduct=1`,
-	SALE: (productId) => `https://www.binance.com/en/nft/goods/sale/${productId}`,
+  LOGIN: "https://accounts.binance.com/en/login",
+  PRODUCT: (productId) =>
+    `https://www.binance.com/en/nft/goods/detail?productId=${productId}&isProduct=1`,
+  SALE: (productId) => `https://www.binance.com/en/nft/goods/sale/${productId}`,
 };
 
 const api = {
-	AUTH: "https://www.binance.com/bapi/accounts/v1/public/authcenter/auth",
-	PRODUCT_DETAIL:
-		"https://www.binance.com/bapi/nft/v1/friendly/nft/nft-trade/product-detail",
-	ORDER_CREATE:
-		"https://www.binance.com/bapi/nft/v1/private/nft/nft-trade/order-create",
-	PRODUCT_ONSLACE:
-		"https://www.binance.com/bapi/nft/v1/private/nft/nft-trade/product-onsale",
-	CHECK: "https://www.binance.com/bapi/nft/v1/private/nft/compliance/check",
+  AUTH: "https://www.binance.com/bapi/accounts/v1/public/authcenter/auth",
+  PRODUCT_DETAIL:
+    "https://www.binance.com/bapi/nft/v1/friendly/nft/nft-trade/product-detail",
+  ORDER_CREATE:
+    "https://www.binance.com/bapi/nft/v1/private/nft/nft-trade/order-create",
+  PRODUCT_ONSLACE:
+    "https://www.binance.com/bapi/nft/v1/private/nft/nft-trade/product-onsale",
+  CHECK: "https://www.binance.com/bapi/nft/v1/private/nft/compliance/check",
 };
 
 module.exports = { api, pages };
