2409515a3a3ca8f737946cbdf01ff6e85cb96cfa
noxend <noxios5674@gmail.com>
2022-01-06 22:11:55 +0200

    test

--- src/config.js
+++ src/config.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 
+const { modes } = require("./constants");
+
 const config = {
-	GOOGLE_KEY: "6LeUPckbAAAAAIX0YxfqgiXvD3EOXSeuq0OpO8u_",
-	TWO_CAPTCHA_KEY: "547f077bae7fd0fd67474d93b85c2636",
-	COUNT_REQUESTS: process.env.COUNT_REQUESTS || 1,
-	NFT_ID: process.env.NFT_ID,
-	NFT_SALE_ID: 1,
+  GOOGLE_KEY: "6LeUPckbAAAAAIX0YxfqgiXvD3EOXSeuq0OpO8u_",
+  TWO_CAPTCHA_KEY: "547f077bae7fd0fd67474d93b85c2636",
+  DELAY_BETWEN_REQUESTS: process.env.DELAY_BETWEN_REQUESTS || 10,
+  COUNT_REQUESTS: process.env.COUNT_REQUESTS || 1,
+  MODE: process.env.MODE || modes.MARKETPLACE,
+  NFT_ID: process.env.NFT_ID,
+  NFT_SALE_ID: 1,
 };
 
 module.exports = config;
